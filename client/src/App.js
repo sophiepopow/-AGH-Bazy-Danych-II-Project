c1cf2d684f93f96fdff244aa3f78217c6e8da2b2
Klaudia Popow <401462@student.agh.edu.pl>
2022-05-08 00:57:24 +0200

    Adding admin panel with mocked user and adding products form

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,27 @@ import { HomePage } from './pages/HomePage/HomePage'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar/NavBar'
 import Stores from './pages/Stores/Stores'
+import Products from './pages/Products/Products'
 import CustomersList from './pages/CustomerList/CustomersList'
 import CustomersInsert from './pages/RegisterPage/CustomersInsert'
 import CustomersUpdate from './pages/CustomerUpdate/CustomersUpdate'
 import LoginPage from './pages/LoginPage/Login'
+import { AdminPanelPage } from './pages/AdminPanelPage/AdminPanelPage'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
     return (
         <Router>
             <NavBar />
+            <ToastContainer />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/register" element={<CustomersInsert />} />
+                <Route path="/products" element={<Products />} />
                 <Route path="/stores" element={<Stores />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/admin" element={<AdminPanelPage />} />
 
 
                 {/* TO-DO: obgadac ponizsze routy */}
---
+++ client/src/pages/AdminPanelPage/AdminPanelPage.js
@@ -0,0 +1,56 @@
+import styles from './AdminPanelPage.module.css';
+import api from '../../api'
+import Button from '@mui/material/Button';
+import { TextField, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
+export const AdminPanelPage = () => {
+    const [category, setCategory] = useState("Vegetable");
+    const [productName, setProductName] = useState("");
+    const [price, setPrice] = useState(0);
+
+    const submitProduct = () => {
+        // send POST req 
+        setCategory("Vegetable");
+        setProductName("");
+        setPrice(0);
+        toast("Succesfully added product!");
+    }
+    return (
+        <div>
+            <div>
+                Add Product
+            </div>
+            <div>
+                <FormControl>
+                    <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                    <Select
+                        value={category}
+                        label="Category"
+                        onChange={(evt) => { setCategory(evt.target.value) }}
+                    >
+                        <MenuItem value={"Vegetable"}>Veegtable</MenuItem>
+                        <MenuItem value={"Fruit"}>Fruit</MenuItem>
+                    </Select>
+                    <TextField
+                        id="outlined-basic"
+                        label="Product name"
+                        variant="outlined"
+                        value={productName}
+                        onChange={(evt) => { setProductName(evt.target.value) }}
+                        />
+                    <TextField 
+                        id="outlined-basic"
+                        label="Price"
+                        variant="outlined"
+                        type="number"
+                        value={price}
+                        onChange={(evt) => { setPrice(evt.target.value) }}
+                        />
+                    <Button variant="contained" color="success" onClick={submitProduct}>Submit</Button>
+                </FormControl>
+            </div>
+        </div>
+    )
+}
