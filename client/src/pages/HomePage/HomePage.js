4e7851aafc41d895c4cb3d0229cad35b5cd19e9e
Klaudia Popow <401462@student.agh.edu.pl>
2022-05-07 20:59:30 +0200

    register minor changes

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import styles from './HomePage.module.css';
 
 export const HomePage = () => {
-    return <div className={styles.container}> <img className={styles.photo} src="cow.jpeg"/></div>
+    return <div className={styles.container}></div>
 }
